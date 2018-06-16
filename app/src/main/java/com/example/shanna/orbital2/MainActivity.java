50a0481989177677441296196ab1681b786cc670
ooihy <huiyingooi1@gmail.com>
2018-06-16 15:07:37 -0400

    Added the toolbar where users can click on to open the navigation drawer, comprising of links to myAccount,  Settings and logout.

--- app/src/main/java/com/example/shanna/orbital2/MainActivity.java
+++ app/src/main/java/com/example/shanna/orbital2/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.shanna.orbital2;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -45,21 +51,64 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        //The 5 lines below is for the 3 horizontal lines for sliding drawer.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
         // Check if user is logged in
-        // If user is not logged in, direct user to login page
+        // If user is not logged in, direct user to login page. Else, will stay at homepage.xml
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
             finish(); //prevent activities from stacking up: once prev activity no needed, finish()
         }
+
+        //for notifications
+/*
+        private void createNotificationChannel(){
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = getString(R.string.channel_name);
+                String description = getString(R.string.channel_description);
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                channel.setDescription(description);
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+
+        //not completed
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+*/
+
     }
+
+    /*
+
+       This is the 3 vertical dots at the side: Like Menu. Currently dont need this, because we have
+       navigation drawer.
 
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             getMenuInflater().inflate(R.menu.left_menu, menu);
             return super.onCreateOptionsMenu(menu);
         }
-
+    */
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
             switch (item.getItemId()) {
